5cb8eea9ed611fd5b1590fe707d835823af5c074
Duraesz <pablosantosduraes6@gmail.com>
2026-04-30 20:21:11 -0300

    3004 pt2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { useFonts } from "expo-font";
 
 
 const geistSans = Geist({
